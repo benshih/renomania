4b46c8837fe53b7f0eef4754de92f01a03ce4a11
freefood89 <freefood89@defc21c0-8c9e-11de-b664-3b115b7b7a9b>
2009-08-30 21:06:36 +0000

    Created RenLCD.c and RenLCD.h

--- trunk/C 18/lcd/lcd_counter.c
+++ trunk/C 18/lcd/lcd_counter.c
@@ -5,6 +5,36 @@
 
 #pragma config FOSC = INTIO67, LVP = OFF
 
+//*****************************
+//		FUNCTION PROTOTYPES
+//*****************************
+
+void InitializeUART(void);
+void SendUART(char*);
+void SendUARTchar(char);
+
+void InitializeLCD(void);
+void WriteLCD(char*);
+void CommandLCD(char);
+void SetLine1 (void);
+void SetLine2 (void);
+void ClearLCD (void);
+
+//*****************************
+//		FUNCTIONS
+//*****************************
+
+void InitializeUART()
+{
+	SPBRG = 25;				//Baud Rate 9600 for 4MHz
+	TRISC = TRISC | 0xC0;	//RX & TX TRIS controls to 1
+	TXSTAbits.SYNC = 0;		//asynchronous operation
+	RCSTAbits.SPEN = 1;		//TX/CK I/O pins as output
+	TXSTAbits.TX9 = 0;		//8-bit transmission
+	BAUDCONbits.CKTXP = 0;	//no data polarity
+	TXSTAbits.TXEN = 1;		//enables transmitter circuitry
+
+}
 void SendUART(char *c)
 {
 	char temp;
@@ -17,14 +47,28 @@ void SendUART(char *c)
 		Delay1KTCYx(5);
 	} while( c[i] != '\0' );
 }
-
 void SendUARTchar(char c)
 {
 	TXREG = c;
 	Delay1KTCYx(5);
 }
 
-void WriteLCD(char *c)
+void InitializeLCD()
+{
+	Delay1KTCYx(20);		//20ms
+	PORTB = 0x00;
+
+	//Function set
+	CommandLCD(0x38);		//8-bit Data Line, 2-lines, 5x8 dots
+	Delay1KTCYx(20);		//20ms
+	ClearLCD();
+	Delay1KTCYx(20);		//20ms
+	//Entry Mode Set
+	CommandLCD(0x06);		//Cursor right, Shift OFF
+	Delay10KTCYx(100);		//1s		
+	CommandLCD(0x0C);		//Display ON, Cursor OFF
+}
+void WriteLCD(char *c)		//Writes Characters to LCD
 {
 	int i = 0;
 	PORTAbits.RA7 = 1;
@@ -37,7 +81,7 @@ void WriteLCD(char *c)
 	}
 }
 
-void CommandLCD(char c)
+void CommandLCD(char c)		//Sends Commands to LCD
 {
 	PORTB=c;				//Line2
 	PORTAbits.RA7 = 0;		//
@@ -46,20 +90,25 @@ void CommandLCD(char c)
 	PORTAbits.RA2 = 0;
 	PORTB = 0x00;
 }
-void SetLine1()
+
+void SetLine1()				//Set cursor to beginning of first line
 {
 	CommandLCD(0x80);
 }
 
-void SetLine2()
+void SetLine2()				//Set cursor to beginning of second line
 {
 	CommandLCD(0xC0);
 }
 
-void ClearLCD()
+void ClearLCD()				//clear the whole screen
 {
 	CommandLCD(0x01);
 }
+
+//*****************************
+//		MAIN
+//*****************************
 
 void main(void)
 {
@@ -73,30 +122,8 @@ void main(void)
 	TRISA = 0x00;			//PORTA output
 	TRISB = 0x00;			//PORTB output
 
-	SPBRG = 25;				//Baud Rate 9600 for 16MHz
-	TRISC = TRISC | 0xC0;	//RX & TX TRIS controls to 1
-	TXSTAbits.SYNC = 0;		//asynchronous operation
-	RCSTAbits.SPEN = 1;		//TX/CK I/O pins as output
-	TXSTAbits.TX9 = 0;		//8-bit transmission
-	BAUDCONbits.CKTXP = 0;	//no data polarity
-	TXSTAbits.TXEN = 1;		//enables transmitter circuitry
-
-	Delay1KTCYx(20);		//20ms
-	PORTB = 0x00;
-
-	//Function set
-	CommandLCD(0x38);		//8-bit Data Line, 2-lines, 5x8 dots
-	Delay1KTCYx(20);		//20ms
-
-	ClearLCD();
-	Delay1KTCYx(20);		//20ms
-
-	//Entry Mode Set
-	CommandLCD(0x06);		//Cursor right, Shift OFF
-	Delay1KTCYx(20);		//20ms
-
-	Delay10KTCYx(100);		//1s		
-	CommandLCD(0x0C);		//Display ON, Cursor OFF
+	InitializeUART();
+	InitializeLCD();
 	
 	for( j = 300; j >= 0; j-- )
 	{
