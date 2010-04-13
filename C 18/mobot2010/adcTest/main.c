10bf929895c43b611bbab89d67177af0d0b68efb
freefood89 <freefood89@defc21c0-8c9e-11de-b664-3b115b7b7a9b>
2010-04-13 22:58:06 +0000

    Arrays for sensor data

--- C 18/mobot2010/adcTest/main.c
+++ C 18/mobot2010/adcTest/main.c
@@ -4,66 +4,58 @@
 //*****************************
 //		Global Variables
 //*****************************
-char ran1, ran2, flag;
-unsigned short int adc1;
+char ran1, ran2;
+char flag;
+//Initialize
+unsigned short int black[10]	= {0,0,0,0,0,0,0,0,0,0};
+unsigned short int white[10] 	= {0,0,0,0,0,0,0,0,0,0};
+unsigned short int thres[10]	= {0,0,0,0,0,0,0,0,0,0};
+unsigned char line[10]			= {0,0,0,0,0,0,0,0,0,0};
 
 //*****************************
 //		INTERRUPT PROTOTYPES
 //*****************************
 #pragma config FOSC = INTIO67, LVP = OFF
-
-#pragma code high_vector = 0x08         //setup the high ISR vector
-void interrupt_at_high_vector(void) {
-	_asm GOTO high_isr _endasm
-}
+#pragma code high_vector = 0x08	//setup the high ISR vector
+void interrupt_at_high_vector(void) 
+	{_asm GOTO high_isr _endasm}
 #pragma code
 #pragma interrupt high_isr      //the high ISR
-void high_isr (void) {
+void high_isr(void){
 	INTCONbits.TMR0IF = 0;
 	TMR0H = 0x0B;
 	TMR0L = 0xDB;
    	if(ran2 == 0) ran2 = 1;
     else ran2 = 0;
 }
-
-#pragma code low_vector = 0x18          //setup the low ISR vector
-void interrupt_at_low_vector(void) {
-     _asm GOTO low_isr _endasm
-}
+#pragma code low_vector = 0x18	//setup the low ISR vector
+void interrupt_at_low_vector(void) 
+	{_asm GOTO low_isr _endasm}
 #pragma code
 #pragma interruptlow low_isr    //the low ISR
-void low_isr (void)	{
+void low_isr(void){
 	PIR1bits.TMR1IF = 0;
 	TMR1H = 0x0B;
 	TMR1L = 0xDB;	
 	if(ran1 == 0) ran1 = 1;
 	else ran1 = 0;
 }
-
 //*****************************
 //		MAIN FUNCTION
 //*****************************
-
 void main(void)
 {
-	//RA0:AN0   RA1:AN1   RA2:AN2   RA3:AN3
-	//RA5:AN4   RB2:AN8   RB3:AN9   RB1:AN10
-	//RB4:AN11	RB0:AN12
-	
-    OSCCONbits.IRCF0 = 1;   //4MHz internal oscillator
-    OSCCONbits.IRCF1 = 0;   //T = 1ms
-    OSCCONbits.IRCF2 = 1;
+	//RA0:AN0   RA1:AN1   RA2:AN2   RA3:AN3   RA5:AN4
+	//RB2:AN8   RB3:AN9   RB1:AN10  RB4:AN11  RB0:AN12
+    initCLK(OSC_4MHZ);
 	initTMR0();
 	initPORTS(); 	
 	initADC();
 	T0CONbits.TMR0ON = 1;
 while(1){
- 		PORTB = (ran2<<5);
- 		SetChanADC(ADC_CH1);
- 		ConvertADC();
+ 		SetChanADC(ADC_CH1); ConvertADC();
  		while(BusyADC());
- 		adc1 = ReadADC();
- 		if(adc1 > 512) PORTAbits.RA0=1;
-	 	else PORTAbits.RA0=0;		 		
+ 		if(ReadADC() > thres[0]) line[0] = 1;
+	 	else line[0] = 0;		 		
  	}
 }
