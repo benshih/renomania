ac3956c88fec0437c360dc5a4832d75ea817bc18
freefood89 <freefood89@defc21c0-8c9e-11de-b664-3b115b7b7a9b>
2011-04-06 23:35:19 +0000

    fixed RenLCD.c: characters are no longer dropped reliability achieved with delays. Fosc must be at least 4Mhz to guarantee proper operation.

--- trunk/C 18/hbridge/main.c
+++ trunk/C 18/hbridge/main.c
@@ -5,6 +5,8 @@
 
 void main(void)
 {
+	char a = 0;
+	char temp = 0;
 	OSCCONbits.IRCF0 = 1;	//4MHz internal oscillator
 	OSCCONbits.IRCF1 = 0;	//T = 1ms 
 	OSCCONbits.IRCF2 = 1;
@@ -16,47 +18,64 @@ void main(void)
 	
 	// 1 and 4
 	// 2 and 3
-//	while(1)
-//	{
 		PORTBbits.RB0=0; //right PMOS off
 		PORTBbits.RB1=0; //left PMOS off
 		PORTBbits.RB2=1; //right NMOS off
 		PORTBbits.RB3=1; //left NMOS off
 		PORTB=0x0C;
-		Delay10KTCYx(1000);
+		Delay10KTCYx(5);
+	while(1)
+	{
+	//	if(a==20) a = 0;
+//		if(a>=20) temp = 40 - a;
+//		else if(a<20) temp = a;
+//		else if(a>40) a=0;
+//		LATBbits.LATB0=1 ; //right PMOS on
+//		LATBbits.LATB1=0; //left PMOS off
+//		LATBbits.LATB2=1; //right NMOS off
+//		LATBbits.LATB3=0; //left NMOS on
+		if(a>40){
+			a=1;
+		}	
+		
+		if(a>20){
+			LATB=0x05;
+			Delay1KTCYx(40-a);
+			LATB=0x0C;
+			Delay1KTCYx(a-20);
+		}	
+		else if(a<=20){
+			LATB=0x0A;
+			Delay1KTCYx(20-a);
+			LATB=0x0C;
+			Delay1KTCYx(a);
+		}	
+		
 		//while(1);
-		PORTBbits.RB0=1 ; //right PMOS on
-		PORTBbits.RB1=0; //left PMOS off
-		PORTBbits.RB2=1; //right NMOS off
-		PORTBbits.RB3=0; //left NMOS on
-		PORTB=0x05;
-		Delay10KTCYx(1000);
-		//while(1);
-		PORTBbits.RB0=0 ; //right PMOS off
-		PORTBbits.RB1=0; //left PMOS off
-		PORTBbits.RB2=1; //right NMOS off
-		PORTBbits.RB3=1; //left NMOS off
-		//PORTB=0x3A;
-		PORTA=0x0C;
-		Delay10KTCYx(1000);
-		PORTBbits.RB0=0 ; //right PMOS off
-		PORTBbits.RB1=1; //left PMOS on
-		PORTBbits.RB2=0; //right NMOS on
-		PORTBbits.RB3=1; //left NMOS off
-		PORTB=0x0A;
-		Delay10KTCYx(1000);
-		PORTBbits.RB0=0 ; //right PMOS off
-		PORTBbits.RB1=0; //left PMOS off
-		PORTBbits.RB2=1; //right NMOS off
-		PORTBbits.RB3=1; //left NMOS off
-		PORTB=0x0C;
-		Delay10KTCYx(1000);
-		while(1)
-		PORTAbits.RA7=0; //right PMOS off
-		PORTAbits.RA2=0; //left PMOS off
-		PORTAbits.RA4=1; //right NMOS off
-		PORTAbits.RA3=1; //left NMOS off*/
-//	}	
+//		PORTBbits.RB0=0 ; //right PMOS off
+//		PORTBbits.RB1=0; //left PMOS off
+//		PORTBbits.RB2=1; //right NMOS off
+//		PORTBbits.RB3=1; //left NMOS off
+
+		
+		
+//		LATBbits.LATB0=0 ; //right PMOS off
+//		LATBbits.LATB1=1; //left PMOS on
+//		LATBbits.LATB2=0; //right NMOS on
+//		LATBbits.LATB3=1; //left NMOS off
+		
+
+	//	LATB=0x0A;
+	//	Delay1KTCYx(a);
+		a++;
+//		PORTBbits.RB0=0 ; //right PMOS off
+//		PORTBbits.RB1=0; //left PMOS off
+//		PORTBbits.RB2=1; //right NMOS off
+//		PORTBbits.RB3=1; //left NMOS off
+//		PORTB=0x0C;
+//		Delay10KTCYx(5);
+
+	}	
 	while(1);
 
 }
