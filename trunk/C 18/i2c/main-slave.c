0dd89c47acfcade9e4fbc3ef388595291d0b3203
freefood89 <freefood89@defc21c0-8c9e-11de-b664-3b115b7b7a9b>
2011-04-13 03:05:39 +0000

    interrupt handlers inserted

--- trunk/C 18/i2c/main-slave.c
+++ trunk/C 18/i2c/main-slave.c
@@ -1,4 +1,5 @@
 #include <p18f25k20.h>
+#include <timers.h>
 #include <i2c.h>
 
 #define LED0 LATAbits.LATA0
@@ -9,17 +10,49 @@
 
 unsigned char var;
 
+//*****************************
+//     FUNCTION PROTOTYPES
+//*****************************
+
 void init(void);
+void initTimers(void);
+void low_isr(void);
+void high_isr(void);
+
+/************************
+       INTERRUPT SETUP
+************************/
+
+#pragma config FOSC = INTIO67, LVP = OFF
+
+#pragma code high_vector = 0x08  //setup the high ISR vector
+void int_high(void) {_asm GOTO high_isr _endasm}
+#pragma code
+#pragma interrupt high_isr      //the high ISR
+
+#pragma code low_vector = 0x18   //setup the low ISR vector
+void int_low(void) {_asm GOTO low_isr _endasm}
+#pragma code
+#pragma interruptlow low_isr    //the low ISR
+
+//*****************************
+//              MAIN
+//*****************************
 
 void main(){
 	init();
+//	initTimers();
 	LED0 = 1; //indicator lamp on
 	while(1){
-		if(DataRdyI2C()){
-			var = getcI2C();
-		}	
+//		if(DataRdyI2C()){
+//			var = getcI2C();
+//		}	
 	}		
 }	
+
+/*******************************
+	     HELPER FUNCTIONS
+********************************/
 
 void init(){
 	//set internal oscillator speed
@@ -30,3 +63,49 @@ void init(){
 	TRISB = 0x00; //all output
 	TRISC = 0x18; //all but RC3,4 are output
 }	
+
+void high_isr (void) {
+	INTCONbits.TMR0IF = 0;
+}
+
+void low_isr (void)	{
+	PIR1bits.TMR1IF = 0;
+}
+
+void initTimers()
+{
+	//turn timers off
+	T0CONbits.TMR0ON = 0;
+    T1CONbits.TMR1ON = 0;
+	
+	//Initializing Timer 0 settings
+	OpenTimer0(	TIMER_INT_ON 	&
+				T0_16BIT 		&	
+				T0_SOURCE_INT 	&
+				T0_EDGE_RISE	&
+				T0_PS_1_16);	
+    T0CONbits.PSA = 0; //prescale off
+	
+	//Initializing Timer 1 settings
+//    OpenTimer1( TIMER_INT_ON	&
+//    			T1_16BIT_RW		&
+//    			T1_SOURCE_INT	&
+//    			T1_PS_1_1		&
+//    			T1_OSC1EN_OFF	&
+//    			T1_SYNC_EXT_OFF);
+//    			
+    //Initializing Interrupts
+    RCONbits.IPEN = 1;      //Initialize the IP
+	INTCON2bits.TMR0IP = 1; //Set Timer0 to HighP
+    IPR1bits.TMR1IP = 0;    //Set Timer1 to LowP
+   	INTCONbits.TMR0IE = 1;  //Enable TMR0 interrupt
+    PIE1bits.TMR1IE = 0;    //Disable TMR1 interrupt    
+    INTCONbits.PEIE = 0;    //Turn off LP interrupts
+    INTCONbits.GIE = 1;     //Turn on HP interrupts
+    
+    WriteTimer0(0x8000);
+//   	WriteTimer1(invert(dur));
+    
+    T0CONbits.TMR0ON = 1;
+    T1CONbits.TMR1ON = 0;
+}
