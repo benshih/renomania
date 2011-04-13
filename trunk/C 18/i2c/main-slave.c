9fc8485e3b83e216d991fd842c1e23924805423a
freefood89 <freefood89@defc21c0-8c9e-11de-b664-3b115b7b7a9b>
2011-04-13 10:38:10 +0000

    works but dont know why

--- trunk/C 18/i2c/main-slave.c
+++ trunk/C 18/i2c/main-slave.c
@@ -45,10 +45,16 @@ void int_low(void) {_asm GOTO low_isr _endasm}
 void main(){
 	init();
 	initI2C();
-//	initTimers();
+	initTimers();
 	LED0 = 1; //indicator lamp on
 	LED1 = 1;
-	while(1);
+	data=getcI2C();	
+	while(1){
+		if(DataRdyI2C()){
+			if(ReadI2C()==3) LED1=0;
+		}	
+		
+	}		
 }	
 
 /*******************************
@@ -68,12 +74,16 @@ void init(){
 void high_isr (void) {
 	//INTCONbits.TMR0IF = 0;
 	PIR1bits.SSPIF = 0;
-	LED1=0;
-  	if(getcI2C()==3) LED0 = 0;
+//	LED1=0;
+	//if(SSPCON1bits.SSPOV) LED1=0;
+  	if(getcI2C()==4) LED0 = 0;
 }
 
 void low_isr (void)	{
 	PIR1bits.TMR1IF = 0;
+	WriteTimer1(0xf000);
+	SSPCON1bits.SSPEN = !SSPCON1bits.SSPEN;
+	LED1 = !LED1;
 }
 
 void initTimers()
@@ -82,48 +92,50 @@ void initTimers()
 	T0CONbits.TMR0ON = 0;
     T1CONbits.TMR1ON = 0;
 	
-	//Initializing Timer 0 settings
-	OpenTimer0(	TIMER_INT_ON 	&
-				T0_16BIT 		&	
-				T0_SOURCE_INT 	&
-				T0_EDGE_RISE	&
-				T0_PS_1_16);	
-    T0CONbits.PSA = 0; //prescale off
+//	//Initializing Timer 0 settings
+//	OpenTimer0(	TIMER_INT_ON 	&
+//				T0_16BIT 		&	
+//				T0_SOURCE_INT 	&
+//				T0_EDGE_RISE	&
+//				T0_PS_1_16);	
+//    T0CONbits.PSA = 0; //prescale off
 	
-	//Initializing Timer 1 settings
-//    OpenTimer1( TIMER_INT_ON	&
-//    			T1_16BIT_RW		&
-//    			T1_SOURCE_INT	&
-//    			T1_PS_1_1		&
-//    			T1_OSC1EN_OFF	&
-//    			T1_SYNC_EXT_OFF);
-//    			
+//	Initializing Timer 1 settings
+    OpenTimer1( TIMER_INT_ON	&
+    			T1_16BIT_RW		&
+    			T1_SOURCE_INT	&
+    			T1_PS_1_1		&
+    			T1_OSC1EN_OFF	&
+    			T1_SYNC_EXT_OFF);
+    			
     //Initializing Interrupts
     RCONbits.IPEN = 1;      //Initialize the IP
 	INTCON2bits.TMR0IP = 1; //Set Timer0 to HighP
     IPR1bits.TMR1IP = 0;    //Set Timer1 to LowP
    	INTCONbits.TMR0IE = 0;  //Enable TMR0 interrupt
-    PIE1bits.TMR1IE = 0;    //Disable TMR1 interrupt    
+    PIE1bits.TMR1IE = 1;    //Enable TMR1 interrupt    
 
     
     WriteTimer0(0x8000);
-//   	WriteTimer1(invert(dur));
+    WriteTimer1(0xf000);
     
     T0CONbits.TMR0ON = 0;
-    T1CONbits.TMR1ON = 0;
+    T1CONbits.TMR1ON = 1;
 }
 
 void initI2C(void){
+	SSPCON1bits.SSPEN = 1;
+	
 	SSPCON1bits.SSPM0 = 0; //I am a slave
 	SSPCON1bits.SSPM1 = 1; //I am a slave
 	SSPCON1bits.SSPM2 = 1; //I am a slave
-	SSPCON1bits.SSPM3 = 1; //I am a slave
+	SSPCON1bits.SSPM3 = 0; //I am a slave
 	SSPADD = 0xff << 1; //enter slave address
 	
 	
 	PIE1bits.SSPIE = 1;	//enable MSSP interrupts
 	IPR1bits.SSPIP = 1; //MSSP are high priority	
 	
-	INTCONbits.PEIE = 0;    //Turn off LP interrupts
+	INTCONbits.PEIE = 1;    //Turn off LP interrupts
     INTCONbits.GIE = 1;     //Turn on HP interrupts
 }	
